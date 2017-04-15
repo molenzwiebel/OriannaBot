664d19fa3ab9832cbde55296a878384e708e2ad9
molenzwiebel <info@thijsmolendijk.nl>
2017-04-15 16:37:53 +0200

    Add database schema, config reading

---
+++ src/database.ts
@@ -0,0 +1,112 @@
+import Based, { Basie, field, children } from "basie";
+
+export interface UserPoints {
+    [champId: number]: number | undefined
+}
+
+export abstract class User extends Basie {
+    @field
+    snowflake: string;
+
+    @field
+    configCode: string;
+
+    @field
+    username: string;
+
+    @field
+    lastUpdateTimestamp: number;
+
+    @children(model => LeagueAccountModel)
+    accounts: LeagueAccount[];
+
+    @field
+    latestPointsJson: string;
+
+    @field
+    optedOutOfReminding: boolean;
+
+    get lastUpdate(): Date {
+        return new Date(this.lastUpdateTimestamp);
+    }
+
+    set lastUpdate(value: Date) {
+        this.lastUpdateTimestamp = value.getTime();
+    }
+
+    get latestPoints(): UserPoints {
+        return JSON.parse(this.latestPointsJson);
+    }
+
+    set latestPoints(value: UserPoints) {
+        this.latestPointsJson = JSON.stringify(value);
+    }
+}
+export const UserModel = Based(User);
+
+export abstract class LeagueAccount extends Basie {
+    @field("user_id")
+    owner: number;
+
+    @field
+    region: string;
+
+    @field
+    accountId: number;
+
+    @field
+    username: string;
+}
+export const LeagueAccountModel = Based(LeagueAccount);
+
+export abstract class DiscordServer extends Basie {
+    @field
+    snowflake: string;
+
+    @field
+    configCode: string;
+
+    @field
+    name: string;
+
+    @field
+    championId: number;
+
+    @field
+    announcePromotions: boolean;
+
+    @field
+    regionRoles: boolean;
+
+    @field("existingRoles")
+    existingRolesJson: string;
+
+    @field
+    mainChannel: string;
+
+    @children(model => RoleModel)
+    roles: Role[];
+
+    @field
+    setupCompleted: boolean;
+
+    set existingRoles(val: string[]) {
+        this.existingRolesJson = JSON.stringify(val);
+    }
+}
+export const DiscordServerModel = Based(DiscordServer);
+
+export abstract class Role extends Basie {
+    @field
+    snowflake: string;
+
+    @field("discordserver_id")
+    owner: number;
+
+    @field
+    name: string;
+
+    @field
+    range: string;
+}
+export const RoleModel = Based(DiscordServer);
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,41 @@
-console.log("Hello, world!");
+import { Database } from "basie";
+import { UserModel, LeagueAccountModel, DiscordServerModel, RoleModel } from "./database";
+import * as fs from "fs";
+
+import debug = require("debug");
+const info = debug("orianna");
+const error = debug("orianna:error");
+
+process.on("unhandledRejection", (err: Error) => {
+    error("Unhandled rejection: %O", err);
+});
+
+interface Configuration {
+    regions: string[];
+    riotApiKey: string;
+
+    ownerSnowflake: string;
+    discordToken: string;
+
+    redditClientId: string;
+    redditClientSecret: string;
+    redditRedirectUrl: string;
+}
+
+(async () => {
+    info("Starting Orianna. Reading config...");
+    if (!fs.existsSync("./config.json")) {
+        error("No config.json found.");
+        return;
+    }
+    const config: Configuration = JSON.parse(fs.readFileSync("./config.json", "utf8"));
+
+    info("Connecting to database.");
+    await Database.connect("./orianna.db");
+
+    info("Preparing database.");
+    await UserModel.createTable();
+    await LeagueAccountModel.createTable();
+    await DiscordServerModel.createTable();
+    await RoleModel.createTable();
+})();
