ba0655625572414158c73d438cfd954c3b539d64
molenzwiebel <info@thijsmolendijk.nl>
2018-03-09 00:00:56 +0100

    [frontend] Start sketching out navbar, desktop only right now

--- frontend/src/components/app/app.vue
+++ frontend/src/components/app/app.vue
@@ -1,5 +1,131 @@
 <template>
-    <div>Hello, world!</div>
+    <div class="contents">
+        <div class="sidebar">
+            <router-link to="/" class="home-logo" exact active-class="ignore">
+                ORIANNA
+            </router-link>
+
+            <div class="divider section"></div>
+
+            <router-link to="/docs">
+                <i class="icon ion-ios-book-outline"></i>
+                Documentation
+            </router-link>
+
+            <div class="divider"></div>
+
+            <a href="https://github.com/molenzwiebel/oriannabot">
+                <i class="icon ion-social-github"></i>
+                Source Code
+            </a>
+
+            <div class="divider"></div>
+
+            <router-link to="/invite">
+                <i class="icon ion-at"></i>
+                Invite Orianna Bot
+            </router-link>
+
+            <div class="divider section"></div>
+
+            <router-link to="/sign-in" class="discord-sign-in">
+                Sign In With
+                <img src="https://discordapp.com/assets/e4923594e694a21542a489471ecffa50.svg">
+            </router-link>
+        </div>
+
+        <div class="view">
+            <router-view></router-view>
+        </div>
+    </div>
 </template>
 
 <script lang="ts" src="./app.ts"></script>
+
+<style lang="stylus">
+    navbar-width = 240px
+    navbar-background = white
+    navbar-text-color = black
+    navbar-selected-color = #3380e5
+
+    *
+        box-sizing border-box
+
+    .contents
+        height 100%
+        width 100%
+
+    .sidebar
+        width navbar-width
+        background-color navbar-background
+        position fixed
+        left 0
+        top 0
+        height 100%
+
+        display flex
+        flex-direction column
+        border-right 1px solid #e5e5e5
+
+        & > a
+            padding 10px 10px 10px 15px
+            transition 0.2s ease
+            text-decoration none
+            position relative
+            font-family Roboto
+            font-size 20px
+            color navbar-text-color
+
+            & > icon
+                width 20px
+
+            &:hover
+                background-color darken(navbar-background, 5%)
+                color navbar-selected-color
+
+            &.router-link-active
+                color navbar-selected-color
+
+            &:before
+                content ''
+                position absolute
+                left 0
+                top 0
+                height 100%
+                width 4px
+                transition 0.2s ease
+
+            &.router-link-active:before
+                background-color navbar-selected-color
+
+        & > .home-logo
+            padding-top 15px
+            font-family Mina
+            text-align center
+            letter-spacing 0.5px
+            font-size 30px
+
+        & > .discord-sign-in
+            padding-top 30px
+            text-align center
+            font-size 16px
+            text-transform uppercase
+            color #898989
+
+            & > img
+                width 160px
+
+        & > .divider
+            width navbar-width
+            height 1px
+            background-color #eaeaea
+
+            &.section
+                background-color #257fca
+                opacity 0.6
+
+    .view
+        margin-left navbar-width
+        height 100%
+        background-color #f5f5f5
+</style>
--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,3 +1,18 @@
+import Vue from "vue";
+import VueRouter from "vue-router";
+
 import App from "./components/app/app.vue";
 
-new App().$mount("#root");
+Vue.use(VueRouter);
+
+const router = new VueRouter({
+    routes: [
+        { path: "/", component: { render(x) { return x("p") } } },
+        { path: "/docs", component: { render(x) { return x("i") } } },
+        { path: "/invite", component: { render(x) { return x("div") } } }
+    ]
+});
+
+new App({
+    router
+}).$mount("#root");
