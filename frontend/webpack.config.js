e4b5223b593a5069281e9f2aafc006d9fd6da6b8
molenzwiebel <info@thijsmolendijk.nl>
2018-04-21 20:22:08 +0200

    [frontend] Upgrade to Webpack 4 for better build times

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -40,6 +40,7 @@ module.exports = env => ({
         port: 8081
     },
     plugins: [
+        new webpack.AutomaticPrefetchPlugin(),
         new webpack.HotModuleReplacementPlugin(),
         new webpack.DefinePlugin({
             GIT_BRANCH: JSON.stringify(gitRevision.branch()),
