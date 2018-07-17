c4583d98e66de651b52b9639cdabe2cb50b72c15
molenzwiebel <info@thijsmolendijk.nl>
2018-07-17 16:01:11 +0200

    [backend] Switch to counting completeness instead of storing a bool

--- backend/src/util/update-loop.ts
+++ backend/src/util/update-loop.ts
@@ -11,20 +11,26 @@ const log = debug("orianna:updater:update_loop");
 export default function scheduleUpdateLoop<T>(handler: (item: T) => Promise<any>, producer: (amount: number) => Promise<T[]>, interval: number, amount: number) {
     // Distribute the jobs over 90% of our interval to give some time for the last jobs to finish.
     const step = Math.ceil((interval * 0.90) / amount);
-    let running = false;
+    let completed = 0;
 
-    setTimeout(async () => {
+    setInterval(async () => {
         // Prevent loops from overlapping and eventually clogging up.
-        if (running) return log("Warning: Update loop took more than %ims to update %i items.", interval, amount);
+        if (completed !== amount) return log("Warning: Update loop took more than %ims to update %i items. So far, %i items are finished.", interval, amount, completed);
 
-        running = true;
+        completed = 0;
         try {
             const items = await producer(amount);
             const promises = items.map((item, i) => new Promise(resolve => {
                 setTimeout(() => {
                     // We catch the error but don't handle it.
                     // This is to prevent the entire Promise.all from aborting when a single reject is received.
-                    handler(item).then(resolve).catch(() => {});
+                    handler(item).then(() => {
+                        completed++;
+                        resolve();
+                    }).catch(() => {
+                        // This still counts as completed, just erroneously :P
+                        completed++;
+                    });
                 }, i * step);
             }));
 
@@ -36,8 +42,9 @@ export default function scheduleUpdateLoop<T>(handler: (item: T) => Promise<any>
 
             log("Error running update loop: %s", e.message);
             log("%O", e);
-        } finally {
-            running = false;
+
+            // Mark as completed so we can continue normally the next cycle.
+            completed = amount;
         }
     }, interval);
 }
