68382bc70080ea22a0d04a7954a5d20c8a1014e8
Thijs Molendijk <info@thijsmolendijk.nl>
2023-01-21 21:16:21 +0100

    [shockwave] Do not assign ranked roles to users without accounts

--- shockwave/shockwave_core/src/evaluate.rs
+++ shockwave/shockwave_core/src/evaluate.rs
@@ -164,6 +164,13 @@ impl TotalMasteryScoreCondition {
 
 impl RankedTierCondition {
     pub fn evaluate(&self, ctx: &EvaluationContext) -> bool {
+        // if the user has no accounts (no ranks), they should not be eligible for anything
+        // this is mostly the case for Unranked users, which should really not have a role
+        // assigned to them if they have no accounts linked
+        if ctx.ranks.is_empty() {
+            return false;
+        }
+
         match &self.queue {
             RankedTierQueue::HighestExcludingTFT | RankedTierQueue::HighestIncludingTFT => {
                 let include_tft = matches!(self.queue, RankedTierQueue::HighestIncludingTFT);
