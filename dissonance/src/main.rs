ef036c7e8091ab99adfca48e32d887d1b250ebfc
Thijs Molendijk <info@thijsmolendijk.nl>
2026-04-01 01:14:50 +0200

    [dissonance] Fix rustls crypto provider

--- dissonance/src/main.rs
+++ dissonance/src/main.rs
@@ -16,6 +16,9 @@ mod worker;
 async fn main() -> Result<(), Box<dyn Error>> {
     tracing_subscriber::fmt::init();
     dotenv::dotenv().ok();
+    rustls::crypto::ring::default_provider()
+        .install_default()
+        .expect("Failed to install rustls crypto provider");
 
     info!("Starting dissonance...");
 
